d8a1d1a37a0efb91fccd3fb072513433335b3785
Kiran Datir <datirkiran0@gmail.com>
2026-05-21 17:05:15 +0530

    initial commit

---
+++ Assignment-03/Program2.c
@@ -0,0 +1,38 @@
+///////////////////////////////////////////////////////////////
+// Write  a Program which accept one number from user and
+// print even factor of that number on screen.
+///////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void DisplayFacter(int iNo)
+{
+    int i = 0;
+
+    if(iNo <= 0) 
+    {
+        iNo = -iNo;
+        return;
+    }
+    for(i = 1; i <= iNo/2; i++)
+    {
+        if((iNo % i) == 0 && (i % 2) == 0)
+        {
+            printf("%d \n", i);
+        }
+    }
+ 
+}
+
+  int main()
+{
+    int iValue = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &iValue);
+
+    DisplayFacter(iValue);
+
+    return 0;
+  
+}
