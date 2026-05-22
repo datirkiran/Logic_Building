4c08a4cd54b6a4211051dca9c0a431e0d33d38a9
Kiran Datir <datirkiran0@gmail.com>
2026-05-22 14:47:55 +0530

    Initial commit

---
+++ Assignment-05/Program2.c
@@ -0,0 +1,31 @@
+//////////////////////////////////////////////////////////////////////
+// Write a Program which accept number for user and print the
+// numbers still that number.
+//
+// input : 5                      
+// Output : 1 2 3 4 5     
+//////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void Display( int iNo) 
+{
+    int iCnt = 0;
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("%d ", iCnt);
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the number : ");
+    scanf("%d", &iValue);
+
+    Display(iValue);
+    printf("\n");
+
+    return 0;
+}
---
+++ Assignment-05/Program5.c
@@ -0,0 +1,31 @@
+//////////////////////////////////////////////////////////////////////
+// Write a Program which accept N number for user and print first 5 
+// multiples of N.
+//
+// input : 5                      
+// Output : 5 10 15 20 25     
+//////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void MultipleDisplay( int iNo) 
+{
+    int iCnt = 0;
+    for(iCnt = 1; iCnt <= 5; iCnt++)
+    {
+        printf("%d ", (iNo * iCnt));
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the number : ");
+    scanf("%d", &iValue);
+
+    MultipleDisplay(iValue);
+    printf("\n");
+
+    return 0;
+}
